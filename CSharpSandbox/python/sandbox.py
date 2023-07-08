ae759f89c2f5c39a49e9e00964d8e5588513a733
Mateusz Zmijewski <mattzy90@hotmail.com>
2023-07-07 22:35:23 -0400

    recursive adding

--- CSharpSandbox/python/sandbox.py
+++ CSharpSandbox/python/sandbox.py
@@ -1,8 +1,14 @@
 def addTwoNumbers(someNumber : int) -> None:
     print(f'Adding two numbers : {someNumber}')
 
-
+def SumOfDigits(num):
+    num = str(num)
+    if len(num) == 1:
+        return int(num)
+    else:
+        return int(num[0]) + SumOfDigits(int(num[1:]))
+    
  
     
 if __name__ == "__main__":
-    addTwoNumbers(123)
+    print(SumOfDigits(123456767))
