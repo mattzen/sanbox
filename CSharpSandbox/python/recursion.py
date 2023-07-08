cf1a5321d83dd71f6d71739c4daf630bc4d21926
Mateusz Zmijewski <mattzy90@hotmail.com>
2023-07-07 22:54:32 -0400

    files split

---
+++ CSharpSandbox/python/recursion.py
@@ -0,0 +1,12 @@
+def SumOfDigits(num : int) -> int:
+    num = str(num)
+    if len(num) == 1:
+        return int(num)
+    return int(num[0]) + SumOfDigits(int(num[1:]))
+    
+def SumofDigitsIterative(num : int) -> int:
+    holder = 0
+    strNum = str(num)
+    for element in strNum:
+        holder += int(element)
+    return holder
--- CSharpSandbox/python/sandbox.py
+++ CSharpSandbox/python/sandbox.py
@@ -1,20 +1,9 @@
+from recursion import *
+
 def addTwoNumbers(someNumber : int) -> None:
     print(f'Adding two numbers : {someNumber}')
 
-def SumOfDigits(num : int) -> int:
-    num = str(num)
-    if len(num) == 1:
-        return int(num)
-    return int(num[0]) + SumOfDigits(int(num[1:]))
-    
-def SumofDigitsIterative(num : int) -> int:
-    holder = 0
-    strNum = str(num)
-    for element in strNum:
-        holder += int(element)
-    return holder
-         
-    
+
 if __name__ == "__main__":
     print(SumOfDigits(123456767))
     print(SumofDigitsIterative(123456767))
