7d3a1614a25fae599b7129f6741e35a4c7ff25f4
Mateusz Zmijewski <mattzy90@hotmail.com>
2023-07-07 22:48:00 -0400

    iterative added

--- CSharpSandbox/python/sandbox.py
+++ CSharpSandbox/python/sandbox.py
@@ -5,10 +5,16 @@ def SumOfDigits(num : int) -> int:
     num = str(num)
     if len(num) == 1:
         return int(num)
-    else:
-        return int(num[0]) + SumOfDigits(int(num[1:]))
+    return int(num[0]) + SumOfDigits(int(num[1:]))
     
- 
+def SumofDigitsIterative(num : int) -> int:
+    holder = 0
+    strNum = str(num)
+    for element in strNum:
+        holder += int(element)
+    return holder
+         
     
 if __name__ == "__main__":
     print(SumOfDigits(123456767))
+    print(SumofDigitsIterative(123456767))
