124bb2c22b5277cd17f7e08d05c68094c9b14066
Mateusz Zmijewski <mattzy90@hotmail.com>
2023-07-08 15:13:00 -0400

    commit

--- CSharpSandbox/python/sandbox.py
+++ CSharpSandbox/python/sandbox.py
@@ -1,9 +1,11 @@
+from ast import List
 from recursion import *
+from majority_element import *
 
-def addTwoNumbers(someNumber : int) -> None:
-    print(f'Adding two numbers : {someNumber}')
 
 
 if __name__ == "__main__":
-    print(SumOfDigits(123456767))
-    print(SumofDigitsIterative(123456767))
+    #print(SumOfDigits(123456767))
+    #print(SumofDigitsIterative(123456767))
+    print(Solution.majorityElement3([1,3,2,2,2,2]))
+    
