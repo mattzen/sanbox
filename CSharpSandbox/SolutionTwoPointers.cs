c76f29dd611702e8178c7fe9f376648f86276a79
you@example.com <you@example.com>
2023-09-07 00:28:12 -0400

    max container solution

--- CSharpSandbox/SolutionTwoPointers.cs
+++ CSharpSandbox/SolutionTwoPointers.cs
@@ -3,14 +3,72 @@ namespace CSharpSandbox
 {
     public class SolutionTwoPointers
     {
+        public int MaxAreaTLE(int[] height)
+        {
+            var result = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                int leftHeight = height[i];
+                for (int j = i + 1; j < height.Length; j++)
+                {
+                    int rightHeight = height[j];
+                    int currentResult;
+
+                    if (leftHeight <= rightHeight)
+                    {
+                        currentResult = leftHeight * (j - i);
+                    }
+                    else
+                    {
+                        currentResult = rightHeight * (j - i);
+                    }
+
+                    if (result < currentResult)
+                    {
+                        result = currentResult;
+                    }
+
+                }
+            }
+            return result;
+        }
+
         public int MaxArea(int[] height)
         {
             var result = 0;
+            int i = 0;
+            int j = height.Length - 1;
 
-            
+            while (i < j)
+            {
 
+                int rightHeight = height[j];
+                int currentResult;
+                int leftHeight = height[i];
 
+                if (leftHeight <= rightHeight)
+                {
+                    currentResult = leftHeight * (j - i);
+                }
+                else
+                {
+                    currentResult = rightHeight * (j - i);
+                }
 
+                if (result < currentResult)
+                {
+                    result = currentResult;
+                }
+                if (leftHeight < rightHeight)
+                {
+                    i++;
+                }
+                if (rightHeight <= leftHeight)
+                {
+                    j--;
+                }
+
+            }
             return result;
         }
     }
