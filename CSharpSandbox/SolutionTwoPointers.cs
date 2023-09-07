26c6adb9f960b11af42fafafa1ec05ce6788c431
you@example.com <you@example.com>
2023-09-06 23:37:22 -0400

    split

---
+++ CSharpSandbox/SolutionTwoPointers.cs
@@ -0,0 +1,17 @@
+using System;
+namespace CSharpSandbox
+{
+    public class SolutionTwoPointers
+    {
+        public int MaxArea(int[] height)
+        {
+            var result = 0;
+
+            
+
+
+
+            return result;
+        }
+    }
+}
